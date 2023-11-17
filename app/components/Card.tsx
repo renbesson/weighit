07abeaa255260718a66869505d029ebeaf730dc7
Ren Besson <ren.besson@outlook.com>
2023-11-17 02:02:03 -0400

    Card buttons styled

--- app/components/Card.tsx
+++ app/components/Card.tsx
@@ -10,7 +10,7 @@ interface Props {
 
 export default function Card({ title, price, weight, image }: Props) {
   return (
-    <div className="card card-side bg-base-100 shadow-xl w-96 image-full">
+    <div className="card card-compact bg-base-100 shadow-xl w-96 image-full">
       <figure>
         <Image width={320} height={240} alt="" src={image || "/images/placeholder.jpg"} />
       </figure>
@@ -18,8 +18,16 @@ export default function Card({ title, price, weight, image }: Props) {
         <h2 className="card-title">{title}</h2>
         <span>Price: ${price}</span>
         <span>Weight: {weight}gr</span>
-        <div className="card-actions justify-end">
-          <button className="btn btn-primary">Edit</button>
+        <div className="absolute bottom-3 right-3">
+          <button className="btn btn-primary btn-circle m-1">
+            <img src="/icons/editImg.svg" />
+          </button>
+          <button className="btn btn-primary btn-circle m-1">
+            <img src="/icons/edit.svg" />
+          </button>
+          <button className="btn btn-error btn-circle m-1">
+            <img src="/icons/delete.svg" />
+          </button>
         </div>
       </div>
     </div>
