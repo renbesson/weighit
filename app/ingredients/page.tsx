05528ac1491495655435878ec4610a8d229c37f9
Ren Besson <ren.besson@outlook.com>
2023-11-30 14:10:18 -0400

    Test on fetch

--- app/ingredients/page.tsx
+++ app/ingredients/page.tsx
@@ -4,6 +4,7 @@ import Link from "next/link";
 import Card from "../components/IngredientCard";
 
 async function getIngrs() {
+  console.log("API_URL: ", process.env.API_URL)
   const res = await fetch(`${process.env.API_URL}/api/ingredients`, {
     cache: "no-store",
   });
