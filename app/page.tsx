fadd650c94b1042821537c9e417c8ff6d44c0eec
Ren Besson <ren.besson@outlook.com>
2023-12-02 14:21:01 -0400

    Testing VERCEL_URL

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="relative flex min-h-screen flex-col items-center justify-center">
-      <pre>{JSON.stringify("ingredients", null, 1)}</pre>
+      <pre>{process.env.VERCEL_URL}</pre>
     </main>
   );
 }
