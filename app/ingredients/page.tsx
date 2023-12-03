1e0a54b68b4fe34459596f7d11b0602eb7344d9b
Ren Besson <ren.besson@outlook.com>
2023-12-02 21:54:37 -0400

    s

--- app/ingredients/page.tsx
+++ app/ingredients/page.tsx
@@ -7,7 +7,7 @@ async function getIngrs() {
   });
 
   if (!res.ok) {
-    throw new Error(`Failed to fetch recipe: ${JSON.stringify(res)}`);
+    throw new Error(`Failed to fetch recipe: ${JSON.stringify(res.json())}`);
   }
 
   return res.json();
